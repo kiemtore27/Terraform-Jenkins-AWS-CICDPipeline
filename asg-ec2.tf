a90de5d01332d433e66613ea511ecafdd1870f9c
kiemtore27 <117327643+kiemtore27@users.noreply.github.com>
2026-03-13 13:30:00 -0400

    Update asg-ec2.tf

--- asg-ec2.tf
+++ asg-ec2.tf
@@ -1,11 +1,10 @@
-# create a launch template
-# terraform aws launch template
+# Create Launch Template
 resource "aws_launch_template" "webserver_launch_template" {
-  name          = var.launch_template-name
+  name          = var.launch_template_name
   image_id      = var.ec2_image_id
-  instance_type = var.ec2_instnace_type
+  instance_type = var.ec2_instance_type
   key_name      = var.ec2_key_pair_name
-  description   = "launch template for asg"
+  description   = "Launch template for ASG"
 
   monitoring {
     enabled = true
@@ -14,14 +13,16 @@ resource "aws_launch_template" "webserver_launch_template" {
   vpc_security_group_ids = [aws_security_group.webserver_security_group.id]
 }
 
-# create auto scaling group
-# terraform aws autoscaling group
+# Create Auto Scaling Group
 resource "aws_autoscaling_group" "auto_scaling_group" {
-  vpc_zone_identifier = [aws_subnet.private_app_subnet_az1.id, aws_subnet.private_app_subnet_az2.id]
-  desired_capacity    = 2
-  max_size            = 4
-  min_size            = 1
-  name                = "dev-asg"
+  vpc_zone_identifier = [
+    aws_subnet.private_app_subnet_az1.id,
+    aws_subnet.private_app_subnet_az2.id
+  ]
+  desired_capacity    = var.asg_desired_capacity
+  max_size            = var.asg_max_size
+  min_size            = var.asg_min_size
+  name                = var.asg_name
   health_check_type   = "ELB"
 
   launch_template {
@@ -31,24 +32,22 @@ resource "aws_autoscaling_group" "auto_scaling_group" {
 
   tag {
     key                 = "Name"
-    value               = "asg-webserver"
+    value               = var.asg_instance_name
     propagate_at_launch = true
   }
 
   lifecycle {
-    ignore_changes      = [target-group-arn]
+    ignore_changes = [target_group_arns]
   }
 }
 
-# attach auto scaling group to alb target group
-# terraform aws autoscaling attachment
+# Attach Auto Scaling Group to ALB Target Group
 resource "aws_autoscaling_attachment" "asg_alb_target_group_attachment" {
   autoscaling_group_name = aws_autoscaling_group.auto_scaling_group.id
   lb_target_group_arn    = aws_lb_target_group.alb_target_group.arn
 }
 
-# create an auto scaling group notification
-# terraform aws autoscaling notification
+# Create Auto Scaling Group Notifications
 resource "aws_autoscaling_notification" "webserver_asg_notifications" {
   group_names = [aws_autoscaling_group.auto_scaling_group.name]
 
