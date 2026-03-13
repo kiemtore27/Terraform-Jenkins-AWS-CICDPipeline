30843b9184a465dd6f1e72bc8db05fc4d8d55aba
kiemtore27 <117327643+kiemtore27@users.noreply.github.com>
2026-03-13 13:10:44 -0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,33 +1,28 @@
-# create application load balancer
-# terraform aws create application load balancer
+# Create Application Load Balancer
 resource "aws_lb" "application_load_balancer" {
-  name               = "dev-alb"
+  name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_security_group.id]
 
-  subnet_mapping {
-    subnet_id = aws_subnet.public_subnet_az1.id
-  }
-
-  subnet_mapping {
-    subnet_id = aws_subnet.public_subnet_az2.id
-  }
+  subnets = [
+    aws_subnet.public_subnet_az1.id,
+    aws_subnet.public_subnet_az2.id
+  ]
 
   enable_deletion_protection = false
 
-  tags   = {
-    Name = "dev-alb"
+  tags = {
+    Name = var.alb_name
   }
 }
 
-# create target group
-# terraform aws create target group
+# Create Target Group
 resource "aws_lb_target_group" "alb_target_group" {
-  name        = "dev-alb"
+  name        = var.target_group_name
   target_type = "instance"
-  port        = 80
-  protocol    = "http"
+  port        = var.target_group_port
+  protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
 
   health_check {
@@ -36,18 +31,17 @@ resource "aws_lb_target_group" "alb_target_group" {
     matcher             = "200,301,302"
     path                = "/"
     port                = "traffic-port"
-    protocol            = "http"
+    protocol            = "HTTP"
     timeout             = 5
     unhealthy_threshold = 2
   }
 }
 
-# create a listener on port 80 with redirect action
-# terraform aws create listener
+# Listener on port 80 with redirect to HTTPS
 resource "aws_lb_listener" "alb_http_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = 80
-  protocol          = "http"
+  protocol          = "HTTP"
 
   default_action {
     type = "redirect"
@@ -55,21 +49,20 @@ resource "aws_lb_listener" "alb_http_listener" {
     redirect {
       host        = "#{host}"
       path        = "/#{path}"
-      port        = 443
-      protocol    = "https"
-      status_code = "http_301"
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
 
-# create a listener on port 443 with forward action
-# terraform aws create listener
+# Listener on port 443 with forward action
 resource "aws_lb_listener" "alb_https_listener" {
-  load_balancer_arn  = aws_lb.application_load_balancer.arn
-  port               = 443
-  protocol           = "https"
-  ssl_policy         = "ELBSecurityPolicy-2016-08"
-  certificate_arn    = var.ssl_certification_arn
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.ssl_certificate_arn
 
   default_action {
     type             = "forward"
