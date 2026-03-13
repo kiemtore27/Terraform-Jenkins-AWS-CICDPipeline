005b6ed506876d504a267b1aa84bb8979dc7b028
kiemtore27 <117327643+kiemtore27@users.noreply.github.com>
2026-03-13 13:53:56 -0400

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,32 +1,32 @@
-# create database subnet group
-# terraform aws db subnet group
+# Create Database Subnet Group
 resource "aws_db_subnet_group" "database_subnet_group" {
-  name         = "database subnets"
-  subnet_ids   = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id]
-  description  = "subnets for database instance"
+  name        = var.db_subnet_group_name
+  subnet_ids  = [
+    aws_subnet.private_data_subnet_az1.id,
+    aws_subnet.private_data_subnet_az2.id
+  ]
+  description = "Subnets for database instance"
 
-  tags   = {
-    Name = "database subnets"
+  tags = {
+    Name = var.db_subnet_group_name
   }
 }
 
-# get the latest db snapshot
-# terraform aws data db snapshot
+# Get the latest DB snapshot
 data "aws_db_snapshot" "latest_db_snapshot" {
-  db_snapshot_identifier = var.database-snapshot-identifier
-  most_recent            = true 
+  db_snapshot_identifier = var.database_snapshot_identifier
+  most_recent            = true
   snapshot_type          = "manual"
 }
 
-# create database instance restored from db snapshots
-# terraform aws db instance
+# Create Database Instance restored from DB snapshot
 resource "aws_db_instance" "database_instance" {
-  instance_class          = var.database-instance_class
-  skip_final_snapshot     = true
-  availability_zone       = "us-east-1b"
-  identifier              = var.database-instance_identifier
-  snapshot_identifier     = data.aws_db_snapshot.latest_db_snapshot.id
-  db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
-  multi_az                = var.multi-az-deployment
-  vpc_security_group_ids  = [aws_security_group.database_security_group.id]
+  instance_class         = var.database_instance_class
+  skip_final_snapshot    = true
+  availability_zone      = var.database_availability_zone
+  identifier             = var.database_instance_identifier
+  snapshot_identifier    = data.aws_db_snapshot.latest_db_snapshot.id
+  db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
+  multi_az               = var.multi_az_deployment
+  vpc_security_group_ids = [aws_security_group.database_security_group.id]
 }
